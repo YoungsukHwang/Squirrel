5653885134e31888a1d66056b603b29130a10ea3
HeshamMasoudi <hhm2122@columbia.edu>
2019-12-08 19:36:58 +0000

    initiated the stats view with some initial stats.

--- tracking/urls.py
+++ tracking/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     #path('map/', views.map),
-    path('sightings/', views.all_squirrels),
-    path('sightings/<unique_squirrel_id>/', views.update_squirrel),
-    path('sightings/add/', views.add_squirrel),
-    #path('sightings/stats/', views.squirrel_stats),
+    path('sightings/', views.all_squirrels), 
+    path('sightings/add/', views.add_squirrel), 
+    path('sightings/stats/', views.squirrel_stats),
+    path('sightings/<squirrel_id>/', views.update_squirrel),
 
 ]
--- tracking/views.py
+++ tracking/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Squirrel
 from .forms import SquirrelForm
@@ -13,13 +13,13 @@ def all_squirrels(request):
 
     #return HttpResponse(text)
 
-def update_squirrel(request, unique_squirrel_id):
-    squirrel = Squirrel.objects.get(unique_squirrel_id=unique_squirrel_id)
+def update_squirrel(request, squirrel_id):
+    squirrel = Squirrel.objects.get(unique_squirrel_id=squirrel_id)
     if request.method == 'POST':
        form = SquirrelForm(request.POST, instance=squirrel)
        if form.is_valid():
            form.save()
-           return redirect(f'/tracking/{unique_squirrel_id}')
+           return redirect(f'/sightings/{squirrel_id}')
         #check data with form
     else:
         form = SquirrelForm(instance=squirrel)
@@ -38,7 +38,7 @@ def add_squirrel(request):
        form = SquirrelForm(request.POST)
        if form.is_valid():
            form.save()
-           return redirect(f'/tracking/')
+           return redirect(f'/sightings/')
         #check data with form
     else:
         form = SquirrelForm()
@@ -50,5 +50,24 @@ def add_squirrel(request):
 
     return render(request,'tracking/edit.html', context)
 
-#def squirrel_stats(request):
+def squirrel_stats(request):
+    #run from human statistic
+    squirrel=0
+    runs_yes = 0
+    runs_no = 0
+    total=len(Squirrel.objects.all())
+    for squirrel in Squirrel.objects.all():
+        if squirrel.runs_from == True:
+            runs_yes += 1
+        else:
+            runs_no += 1
+        
+
+    context = {
+        'runs_yes': runs_yes,
+        'runs_no': runs_no,
+    }
+
+    return render(request, 'tracking/stats.html', context)
+
 # Create your views here.
